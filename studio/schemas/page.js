59c6e7d9c1f08b9e2d5e467004d1f1a45f45ee54
lunaceee <lunaceee@gmail.com>
2020-12-30 00:16:50 -0800

    add page schema

---
+++ studio/schemas/page.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+}
