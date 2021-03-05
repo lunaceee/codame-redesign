34a2f0e961fb7e512cc780a13c87b9a532f4ede8
lunaceee <lunaceee@gmail.com>
2021-03-04 21:51:51 -0800

    install dot.env, set up .env file to access sanity client

---
+++ utils/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || "p38n6r66",
+    dataset: process.env.SANITY_DATASET || "production",
+  },
+};
---
+++ utils/sanityClient.js
@@ -0,0 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
+const sanityClient = require("@sanity/client");
+
+const { sanity } = require("./client-config");
+
+module.exports = sanityClient({
+  ...sanity,
+  useCdn: !process.env.SANITY_READ_TOKEN,
+  token: process.env.SANITY_READ_TOKEN,
+});
